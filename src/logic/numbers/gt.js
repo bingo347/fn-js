2f20ac5f332d7d8a07ca022aab5d1fdc291076e0
Dmitriy Belyaev <dima@seedr.ru>
2018-06-19 12:56:14 +0300

    refac: logic/numbers/gt

--- src/logic/numbers/gt.js
+++ src/logic/numbers/gt.js
@@ -2,8 +2,10 @@ import mapReduceArgs from '../../fn/mapReduceArgs';
 import curry from '../../fn/curry';
 import greater from '../greater';
 
+const gtInternal = mapReduceArgs(Number, greater);
+
 function gt(a, b) {
-    return mapReduceArgs(Number, greater)(a, b);
+    return gtInternal(a, b);
 }
 
 export default curry(gt);
