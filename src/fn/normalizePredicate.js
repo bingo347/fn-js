e45a5ba8a096cf4fd75baf82fd9e682756346da6
Dmitriy Belyaev <dima@seedr.ru>
2018-06-19 11:41:28 +0300

    add: fn/normalizePredicate

---
+++ src/fn/normalizePredicate.js
@@ -0,0 +1,11 @@
+import always from './always';
+import isFunction from '../predicates/isFunction';
+
+function normalizePredicate(predicate) {
+    return (isFunction(predicate)
+        ? predicate
+        : always(!!predicate)
+    );
+}
+
+export default normalizePredicate;
