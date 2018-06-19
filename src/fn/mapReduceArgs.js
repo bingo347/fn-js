3d3e11bc7b273db6ecf93674d7ff8a36bc986145
Dmitriy Belyaev <dima@seedr.ru>
2018-06-19 11:09:02 +0300

    refac: fn/mapReduceArgs

--- src/fn/mapReduceArgs.js
+++ src/fn/mapReduceArgs.js
@@ -1,9 +1,9 @@
 import curry from './curry';
 import only from './only';
 
-function mapReduce(mapper, reducer) {
+function mapReduceArgs(mapper, reducer) {
     const normalizedMapper = only(0, mapper);
     return (...args) => reducer(...args.map(normalizedMapper));
 }
 
-export default curry(mapReduce);
+export default curry(mapReduceArgs);
