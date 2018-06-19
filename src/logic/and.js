28cc5ba3f8840c22b6f8abb19d1c1ca239085cd7
Dmitriy Belyaev <dima@seedr.ru>
2018-06-19 11:46:33 +0300

    add normalization: logic/and

--- src/logic/and.js
+++ src/logic/and.js
@@ -1,5 +1,8 @@
+import normalizePredicate from '../fn/normalizePredicate';
+
 function and(...predicates) {
-    return (...args) => predicates.every(predicate => !!predicate(...args));
+    const normalizedPredicates = predicates.map(normalizePredicate);
+    return (...args) => normalizedPredicates.every(predicate => !!predicate(...args));
 }
 
 export default and;
