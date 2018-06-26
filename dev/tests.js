f43a861ca237b417e5a4fde6748d010a3220d2b7
Dmitriy Belyaev <dima@seedr.ru>
2018-06-26 15:03:35 +0300

    dev: refactor tests

--- dev/tests.js
+++ dev/tests.js
@@ -1,37 +1,9 @@
-import fs from 'fs';
 import path from 'path';
-import {promisify} from 'util';
+import findInDirectory from './findInDirectory';
 
-const fsA = [
-    'readdir',
-    'stat'
-].reduce((acc, key) => {
-    acc[key] = promisify(fs[key]);
-    return acc;
-}, {});
-
-findTestsInDirectory(path.join(__dirname, '../src')).then(filesForTest => {
+findInDirectory(
+    path.join(__dirname, '../src'),
+    filePath => filePath.endsWith('.test.js')
+).then(filesForTest => {
     return filesForTest.map(filePath => require(filePath));
 }).catch(console.error);
-
-async function findTestsInDirectory(dirPath) {
-    const files = await fsA.readdir(dirPath);
-    const filesInfo = await Promise.all(files.map(async(filename) => {
-        const filePath = path.join(dirPath, filename);
-        const stats = await fsA.stat(filePath);
-        return {
-            filePath,
-            isFile: stats.isFile(),
-            isDir: stats.isDirectory()
-        };
-    }));
-    const subDirs = await Promise.all(filesInfo
-        .filter(({isDir}) => isDir)
-        .map(({filePath}) => findTestsInDirectory(filePath))
-    );
-    return (filesInfo
-        .filter(({filePath, isFile}) => isFile && filePath.endsWith('.test.js'))
-        .map(({filePath}) => filePath)
-        .concat(...subDirs)
-    );
-}
