b11ccc6fefea533c150633ddc8fc915674875d62
Dmitriy Belyaev <dima@seedr.ru>
2018-08-15 18:08:47 +0300

    dev: upd indexes

--- dev/indexes.js
+++ dev/indexes.js
@@ -21,9 +21,9 @@ findInDirectory(DIST_PATH).then(files => {
     }
     const dirsNames = Object.getOwnPropertyNames(dirs);
     for(const dir of dirsNames) {
-        const files = Array.from(dirs[dir]);
+        const files = Array.from(dirs[dir]).filter(f => !f.startsWith('_'));
         const subDirs = Array.from(new Set(dirsNames
-            .filter(d => d !== dir && d.startsWith(dir))
+            .filter(d => d !== '/' && d !== dir && d.startsWith(dir))
             .map(d => d.replace(dir, '').split('/')[dir === '/' ? 0 : 1])
         ));
         dirs[dir] = {files, subDirs};
@@ -34,21 +34,22 @@ findInDirectory(DIST_PATH).then(files => {
 async function makeIndex({files, subDirs}, dir) {
     const dirsPreImports = subDirs.map(subDir => {
         return {
-            imp: `import * as __${subDir} from './${subDir}/index.mjs';`,
-            exp: `export const ${subDir} = __${subDir};`
+            imp: `import * as ${subDir} from './${subDir}/index.mjs';`,
+            exp: subDir
         };
     });
     const filePreImports = files.map(file => {
         return {
-            imp: `import __${file} from './${file}.mjs';`,
-            exp: `export const ${file} = __${file};`
+            imp: `import ${file} from './${file}.mjs';`,
+            exp: file
         };
     });
     const dirsImports = dirsPreImports.map(({imp}) => imp).join('\r\n');
-    const dirsExports = dirsPreImports.map(({exp}) => exp).join('\r\n');
+    const dirsExports = dirsPreImports.map(({exp}) => exp).join(',\r\n    ');
     const fileImports = filePreImports.map(({imp}) => imp).join('\r\n');
-    const fileExports = filePreImports.map(({exp}) => exp).join('\r\n');
-    const source = [dirsImports, fileImports, dirsExports, fileExports].filter(code => !!code).join('\r\n\r\n');
+    const fileExports = filePreImports.map(({exp}) => exp).join(',\r\n    ');
+    const allExports = `export {\r\n    ${dirsExports ? dirsExports + ',\r\n    ' : ''}${fileExports}\r\n};`;
+    const source = [dirsImports, fileImports, allExports].filter(code => !!code).join('\r\n');
     const indexFile = path.join(DIST_PATH, dir.slice(1), 'index.mjs');
     await fsA.writeFile(indexFile, source);
     const code = await babelize(indexFile);
