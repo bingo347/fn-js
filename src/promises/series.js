ab1a5b17c82eecb97835a47c99e153c135772abb
Dmitriy Belyaev <dima@seedr.ru>
2018-08-17 17:40:51 +0300

    promises/series: add encapsulatedArgs

--- src/promises/series.js
+++ src/promises/series.js
@@ -1,12 +1,20 @@
-const reducer = (promise, mapper) => promise.then(mapper);
+import makeEncapsulator from '../make/encapsulator';
+
+const reducerEncapsulator = makeEncapsulator().applyTo(
+    (promise, mapper, ...encapsulatedArgs) => promise.then(value => mapper(value, ...encapsulatedArgs)),
+    true
+);
 
 /**
  * @name series
  * @param  {...(value: *) => Promise} mappers
- * @returns {(value: *) => Promise}
+ * @returns {(value: *, ...encapsulatedArgs: *[]) => Promise}
  */
 function series(...mappers) {
-    return value => mappers.reduce(reducer, Promise.resolve(value));
+    return (value, ...encapsulatedArgs) => {
+        const reducer = reducerEncapsulator.encapsulate(...encapsulatedArgs);
+        return mappers.reduce(reducer, Promise.resolve(value));
+    };
 }
 
 export default series;
