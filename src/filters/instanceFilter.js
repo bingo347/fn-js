dac752507ba13c84e60813ff675da21c56d2667d
Dmitriy Belyaev <dima@seedr.ru>
2018-09-13 23:20:41 +0300

    filters/instanceFilter: add types

--- src/filters/instanceFilter.js
+++ src/filters/instanceFilter.js
@@ -1,8 +1,3 @@
-/**
- * @name instanceFilter
- * @param {function} ClassConstructor
- * @returns {(value: *) => boolean}
- */
 function instanceFilter(ClassConstructor) {
     return value => value instanceof ClassConstructor;
 }
