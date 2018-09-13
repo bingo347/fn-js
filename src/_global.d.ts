e555f01a619c5500340439197e5bad1ca083af77
Dmitriy Belyaev <dima@seedr.ru>
2018-09-13 23:24:56 +0300

    fix global types

--- src/_global.d.ts
+++ src/_global.d.ts
@@ -1,7 +1,18 @@
-export type Predicate = (...args: any[]) => boolean;
-export type ValuePredicate<V> = (value: V, ...args: any[]) => boolean;
+export interface Predicate {
+    (...args: any[]): boolean;
+}
+
+export interface ValuePredicate<V> {
+    (value: V, ...args: any[]): boolean;
+}
+
+export interface Mapper<R> {
+    (...args: any[]): R;
+}
+
+export interface ValueMapper<V, R> {
+    (value: V, ...args: any[]): R;
+}
+
 export type Predicatable = Predicate | boolean;
 export type ValuePredicatable<V> = ValuePredicate<V> | boolean;
-
-export type Mapper<R> = (...args: any[]) => R;
-export type ValueMapper<V, R> = (value: V, ...args: any[]) => R;
