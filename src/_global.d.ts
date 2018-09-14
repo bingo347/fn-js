d39a9e0eafb9f81a286208a42b89eb4b2d635f55
Dmitriy Belyaev <dima@seedr.ru>
2018-09-14 16:03:38 +0300

    fix global types

--- src/_global.d.ts
+++ src/_global.d.ts
@@ -1,10 +1,13 @@
 export interface Mapper<V, R> {
     (value: V, ...args: any[]): R;
+    (value: V): R;
+    (): R;
 }
 
 export interface Predicate<V> {
     (value: V, ...args: any[]): boolean;
     (value: V): boolean;
+    (): boolean;
 }
 
 export type Predicatable<V> = Predicate<V> | boolean;
