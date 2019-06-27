d960f983eb151cf953039bb8cdf63d381d5a53dd
Dmitriy Belyaev <mail@d-belyaev.ru>
2019-06-27 05:40:57 +0300

    only: upgrade types

--- src/only.ts
+++ src/only.ts
@@ -1,26 +1,16 @@
 import curry, {CurriedFunction2} from './curry';
 
-type T0<A> = [A, ...any[]];
-type T1<A> = [any, A, ...any[]];
-type T2<A> = [any, any, A, ...any[]];
-type T3<A> = [any, any, any, A, ...any[]];
-type T4<A> = [any, any, any, any, A, ...any[]];
-type T5<A> = [any, any, any, any, any, A, ...any[]];
-type T6<A> = [any, any, any, any, any, any, A, ...any[]];
-type T7<A> = [any, any, any, any, any, any, any, A, ...any[]];
-type T8<A> = [any, any, any, any, any, any, any, any, A, ...any[]];
-type T9<A> = [any, any, any, any, any, any, any, any, any, A, ...any[]];
 type Args<I, A>
-    = I extends 0 ? T0<A>
-    : I extends 1 ? T1<A>
-    : I extends 2 ? T2<A>
-    : I extends 3 ? T3<A>
-    : I extends 4 ? T4<A>
-    : I extends 5 ? T5<A>
-    : I extends 6 ? T6<A>
-    : I extends 7 ? T7<A>
-    : I extends 8 ? T8<A>
-    : I extends 9 ? T9<A>
+    = I extends 0 ? [A, ...any[]]
+    : I extends 1 ? [any, A, ...any[]]
+    : I extends 2 ? [any, any, A, ...any[]]
+    : I extends 3 ? [any, any, any, A, ...any[]]
+    : I extends 4 ? [any, any, any, any, A, ...any[]]
+    : I extends 5 ? [any, any, any, any, any, A, ...any[]]
+    : I extends 6 ? [any, any, any, any, any, any, A, ...any[]]
+    : I extends 7 ? [any, any, any, any, any, any, any, A, ...any[]]
+    : I extends 8 ? [any, any, any, any, any, any, any, any, A, ...any[]]
+    : I extends 9 ? [any, any, any, any, any, any, any, any, any, A, ...any[]]
     : any[];
 
 function only<A, R, I extends number>(argIndex: I, fn: (arg: A) => R): (...args: Args<I, A>) => R {
