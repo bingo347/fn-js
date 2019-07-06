7f0d42de8ded7c4abaade709a53cac4ee73374f6
Dmitriy Belyaev <mail@d-belyaev.ru>
2019-07-06 04:01:42 +0300

    rw2ts: predicates/isString

---
+++ src/predicates/isString.ts
@@ -0,0 +1,28 @@
+import typeFilter from '../filters/typeFilter';
+
+/**
+ * check given value is string
+ * @param value value for check
+ * @returns true if the given value is string
+ *
+ * @example
+ * isString(''); // => true
+ * isString(123); // => false
+ * isString(new String()); // => false
+ */
+const isString = typeFilter('string') as (value: any) => value is string;
+
+export default isString;
+
+// <test>
+import test from 'ava';
+(function() {
+    if(module.parent.id !== '.') { return; }
+    test('typeof', t => {
+        t.true(isString(''));
+        t.false(isString(123));
+        // tslint:disable-next-line: no-construct
+        t.false(isString(new String()));
+    });
+})();
+// </test>
