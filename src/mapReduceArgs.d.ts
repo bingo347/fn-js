ad5b3a60914bcc3655071e37e30cd9f2a6272ed7
Dmitriy Belyaev <mail@d-belyaev.ru>
2019-08-29 19:17:04 +0300

    rw2ts: mapReduceArgs

--- src/mapReduceArgs.d.ts
+++
@@ -1,6 +0,0 @@
-import {Mapper} from "./_global";
-
-declare function mapReduceArgs<V = any, T = any, R = any>(mapper: Mapper<V, T>, reducer: (...args: T[]) => R): (...args: V[]) => R;
-declare function mapReduceArgs<V = any, T = any, R = any>(mapper: Mapper<V, T>): (reducer: (...args: T[]) => R) => (...args: V[]) => R;
-
-export default mapReduceArgs;
---
+++ src/mapReduceArgs.ts
@@ -0,0 +1,38 @@
+import curry from './curry';
+import only0 from './only0';
+import {Mapper} from './types';
+
+function mapReduceArgsInternal<V, T, R>(
+    mapper: Mapper<[V], T>,
+    reducer: (...args: T[]) => R
+): (...args: V[]) => R {
+    const normalizedMapper = only0(mapper);
+    return (...args) => reducer(...args.map(normalizedMapper));
+}
+
+/**
+ * Map every argument by mapper and reduce all by reducer
+ * @param mapper mapper function
+ * @param reducer reducer function
+ * @return function
+ *
+ * @example
+ * const parseFloatAll = mapReduceArgs(a => parseFloat(a), (...args) => args);
+ * parseFloatAll('1', '2', '3') // => [1, 2, 3]
+ */
+const mapReduceArgs = curry(mapReduceArgsInternal);
+
+export default mapReduceArgs;
+
+// <test>
+import test from 'ava';
+(function() {
+    if(module.parent.id !== '.') { return; }
+    test('args mapping', t => {
+        t.deepEqual([1, 2, 3], mapReduceArgs(
+            (a: string) => parseFloat(a),
+            (...args) => args
+        )('1', '2', '3'));
+    });
+})();
+// </test>
